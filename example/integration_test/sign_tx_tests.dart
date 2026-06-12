39dd74abceb90a5e4d347ee92049800a3d71381f
carmenmmartnz <carmen.martinez@vacuumlabs.com>
2026-06-12 11:35:51 +0200

    [test] add pool registration sign-tx integration cases from ledgerjs

--- example/integration_test/sign_tx_tests.dart
+++ example/integration_test/sign_tx_tests.dart
@@ -11,6 +11,7 @@ import 'cases/sign_tx_conway_with_certs.dart';
 import 'cases/sign_tx_mary.dart';
 import 'cases/sign_tx_multi_delegation.dart';
 import 'cases/sign_tx_combined_certs.dart';
+import 'cases/sign_tx_pool_registration.dart';
 import 'cases/sitn_tx_alonzo_trezor_comparison.dart';
 import 'sign_tx_test_cases.dart';
 import 'test_utils.dart';
@@ -134,6 +135,12 @@ void main() async {
 
     testsConwayCombinedCerts.testGroup(
       groupName: 'signTxConwayCombinedCerts',
+      appVersion: appVersion,
+      cardanoApp: cardanoApp,
+    );
+
+    testsPoolRegistration.testGroup(
+      groupName: 'signTxPoolRegistration',
       appVersion: appVersion,
       cardanoApp: cardanoApp,
     );
